afaedc0a5e1b2593caf869b5c9b1633da2d050ef
k-vernooy <kai.a.vernooy@gmail.com>
2019-09-18 17:34:28 -0400

    Begin constructor

--- include/kern.hpp
+++ include/kern.hpp
@@ -6,7 +6,12 @@ using namespace std;
 class Kern {
 
     string title;
+    string file_path;
+    string contents;
 
 public:
+    
+    Kern(string path);
+
     void meta();
 };
--- src/kern.cpp
+++ src/kern.cpp
@@ -6,3 +6,8 @@ using namespace std;
 void Kern::meta() {
     cout << "meta function";
 }
+
+Kern::Kern(string path) {
+    // file_path = path;
+    cout << path << endl;
+}
