6c55427edf8f9e2b4e71ef24919a46116f83a9ab
k-vernooy <kai.a.vernooy@gmail.com>
2019-09-28 16:40:44 -0400

    Added file list

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,33 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    string file = argv[1];
+bool file_exists(const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    }
+    return false;
+}
 
-    Kern kern(file);
-    // for ( int i = 0; i < argc; i++) {
-    //     cout << argv[i] << endl;
-    // }
+int main(int argc, char *argv[]) {
+    vector<string> fileList;
+
+    string usage = "BiB: usage: bib [options] infile ...";
+
+    for ( int i = 1; i < argc; i++ ) {
+        if ( file_exists(argv[i]) ) {
+            cout << argv[i] << " is a file" << endl;
+            fileList.push_back(argv[i]);
+        }
+    }
+
+    if ( fileList.size() == 0 ) {
+        cout << "bib: \e[31merror:\e[0m No input files specified";
+        return 1;
+    }
+    for ( int i = 0; i < fileList.size(); i++) {
+        string filepath = fileList[i];
+        Kern kern(filepath);
+    }
+
 }
